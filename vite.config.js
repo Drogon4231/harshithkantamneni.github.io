4870f494b3e00b855a2c1be8954fbc5736326b11
Drogon4231 <hkantamneni2@wisc.edu>
2025-05-25 13:24:55 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  // 👇 tells Vite every asset lives one folder deep on Pages
+  base: '/harshithkantamneni.github.io/',
+});
